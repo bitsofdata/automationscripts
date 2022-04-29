f0aecfcf87055d36486d2478f0c7187a240f2233
Gustavo Delgado <gaboware@gmail.com>
2022-04-29 08:54:38 -0400

    Create CopyBot.ps1

---
+++ CopyBot.ps1
@@ -0,0 +1,22 @@
+#Script uses Absolute paths
+#Copies Students JPEG photo files from ic-vision SchoolFiles folder to the ic-comm Destiny Folder
+#Calls the BuildMapFile script to build and zip required files
+#Author: grdsystems
+#Date:  04/27/2022 
+#Version: 1.0
+
+$source = "E:\SchoolFiles\"
+$destination = "G:\ED_TECH\Destiny\"
+
+
+#Use windows robocopy utility to traverse SchooFiles folder structure and pull .JPG files, filters out non-photo folders. 
+#future robocopy calls will only copy new or changed files 
+
+Invoke-Expression "robocopy $source $destination *.jpg /E /Z /R:5 /W:5 /TBD /NP /V /XO  /xd 'PHOTOS_old' /xd 'BITMAPS' /xd 'CDFS' /xd 'INI' /xd 'PWD' /xd 'DATA' /xd 'AdminReports' /xd 'Client Setup'"
+
+#Call to BuildMapFile
+
+& $destination\BuildMapFile.ps1
+
+
+
